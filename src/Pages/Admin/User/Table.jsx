4e5e6acde33d84c48ac28eebfff6b29269ddeff3
AlanPratama <109650474+AlanPratama@users.noreply.github.com>
2024-03-08 00:21:42 +0700

    optimized pagination ui for users table for officer

--- src/Pages/Admin/User/Table.jsx
+++ src/Pages/Admin/User/Table.jsx
@@ -401,11 +401,11 @@ export default function Table({ dataUser, setDataUser, meta }) {
             <option>100</option>
           </select> */}
             <ul className="flex space-x-1 ml-2">
-              <li
+              {
+                prev ? 
+                <li
                 onClick={handlePrev}
-                className={`${
-                  prev ? "flex" : "hidden"
-                } items-center justify-center cursor-pointer bg-gray-300 w-7 h-7 rounded`}
+                className={`flex items-center justify-center cursor-pointer bg-gray-300 w-7 h-7 rounded`}
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -418,6 +418,22 @@ export default function Table({ dataUser, setDataUser, meta }) {
                   />
                 </svg>
               </li>
+              :
+              <li
+                className={`flex items-center justify-center cursor-not-allowed bg-gray-100 w-7 h-7 rounded`}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-3 fill-gray-500"
+                  viewBox="0 0 55.753 55.753"
+                >
+                  <path
+                    d="M12.745 23.915c.283-.282.59-.52.913-.727L35.266 1.581a5.4 5.4 0 0 1 7.637 7.638L24.294 27.828l18.705 18.706a5.4 5.4 0 0 1-7.636 7.637L13.658 32.464a5.367 5.367 0 0 1-.913-.727 5.367 5.367 0 0 1-1.572-3.911 5.369 5.369 0 0 1 1.572-3.911z"
+                    data-original="#000000"
+                  />
+                </svg>
+              </li>
+              }
               {/* <li className="flex items-center justify-center cursor-pointer text-sm w-7 h-7 rounded">
               1
             </li> */}
@@ -427,11 +443,11 @@ export default function Table({ dataUser, setDataUser, meta }) {
               {/* <li className="flex items-center justify-center cursor-pointer text-sm w-7 h-7 rounded">
               3
             </li> */}
-              <li
+              {
+                next ? 
+                <li
                 onClick={handleNext}
-                className={`${
-                  next ? "flex" : "hidden"
-                } items-center justify-center cursor-pointer bg-gray-300 w-7 h-7 rounded`}
+                className={`flex items-center justify-center cursor-pointer bg-gray-300 w-7 h-7 rounded`}
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -444,6 +460,22 @@ export default function Table({ dataUser, setDataUser, meta }) {
                   />
                 </svg>
               </li>
+              :
+              <li
+                className={`flex items-center justify-center cursor-not-allowed bg-gray-100 w-7 h-7 rounded`}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-3 fill-gray-500 rotate-180"
+                  viewBox="0 0 55.753 55.753"
+                >
+                  <path
+                    d="M12.745 23.915c.283-.282.59-.52.913-.727L35.266 1.581a5.4 5.4 0 0 1 7.637 7.638L24.294 27.828l18.705 18.706a5.4 5.4 0 0 1-7.636 7.637L13.658 32.464a5.367 5.367 0 0 1-.913-.727 5.367 5.367 0 0 1-1.572-3.911 5.369 5.369 0 0 1 1.572-3.911z"
+                    data-original="#000000"
+                  />
+                </svg>
+              </li>
+              }
             </ul>
           </div>
         </div>
